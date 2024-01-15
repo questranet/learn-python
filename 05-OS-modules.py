b12f59a5255e9eeb711a4a0bb0395fbd6d0efcbb
Wale <Adewale.megbabi@gmail.com>
2024-01-15 17:47:52 +0100

    Python.Learn

--- 05-OS-modules.py
+++ 05-OS-modules.py
@@ -2,3 +2,10 @@ import os
 
 name = os.environ['HOME']
 print(name)
+
+
+
+
+with open('/tmp/1.txt', 'w') as file:
+    file.write('This is an example file .\n')
+    file.write('It was created using the os module in Python')
