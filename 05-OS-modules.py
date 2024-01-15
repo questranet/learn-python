cebdb114447076f34ffcc536795875022ad12312
Wale <Adewale.megbabi@gmail.com>
2024-01-15 17:51:05 +0100

    Python.Learn

--- 05-OS-modules.py
+++ 05-OS-modules.py
@@ -5,7 +5,8 @@ print(name)
 
 
 
-
+# To create a file using Python OS module
 with open('/tmp/1.txt', 'w') as file:
     file.write('This is an example file .\n')
     file.write('It was created using the os module in Python')
+
